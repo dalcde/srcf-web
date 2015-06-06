0df5fb41dfb692b88a51906d53c14197d5a80209
dalcde <dalcde@yahoo.com.hk>
2015-06-06 19:25:57 +0100

    Improve mobile experience

--- header.php
+++ header.php
@@ -3,7 +3,9 @@
 <head>
   <meta name="description" content="Cambridge Mathematics Lecture Notes" />
   <meta name="author" content="Dexter Chua" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta charset="utf-8" />
+  <link rel="canonical" href="https://dec41.user.srcf.net/<?php echo basename($_SERVER['REQUEST_URI'], ".php") ?>" />
   <link rel="stylesheet" href="style.css" />
   <link rel="shortcut icon" href="favicon.ico?" />
 
