3d248dae02caf01bdfbeed9202519c0b821c9bf5
dalcde <dalcde@yahoo.com.hk>
2016-05-06 21:15:43 +0100

    More updates

--- index.php
+++ index.php
@@ -1,27 +1,41 @@
 <?php
-$title = "My Notes";
+$title = "Maths Lecture Notes";
 include "header.php"?>
 
-<p>Below are the notes I took during lectures in Cambridge. None of this is official (unless otherwise specified). The source code of my notes is also available, which has to be compiled with <a href="notes/header.tex"><code>header.tex</code></a>. It is also available on <a href="http://github.com/dalcde/cam-notes">GitHub</a>.</p>
+<p>Below are the notes I took during lectures in Cambridge. None of this is official (unless otherwise specified). Included as well are stripped-down versions (eg. definition-only; script-generated and doesn't necessarily make sense), example sheets, and the source code. The source code has to be compiled with <a href="notes/header.tex"><code>header.tex</code></a>, and is also available on <a href="http://github.com/dalcde/cam-notes">GitHub</a>.</p>
 
-<p>I have also produced various stripped-down versions (e.g. definition-only) which may be useful for revision. However, keep in mind that they are automatically generated from my regular notes via a poorly-hacked-together shell script and may be nonsensical in certain cases.</p>
-
-<p>For the sake of <a class="hidden" href="http://en.wikipedia.org/wiki/Completeness_(logic)">completeness</a>, the example sheets for the courses are included as well. These are <a class="hidden" href="http://theproofistrivial.com">obviously</a> not my work.</p>
-
-<p>Please email any comments to <a href="mailto:dec41@cam.ac.uk">dec41@cam.ac.uk</a>. Feel free to point out errors or unclear explanations, as well as general typographic suggetsions. Even better, send a GitHub <a href="http://github.com/dalcde/cam-notes/pulls">pull request</a>.</p>
-
-<p><em>Note that I upload my notes as I produce them, and fix and polish them when I have time. Hence they in their current form may or may not be suitable for human consumption. Those that are proven Cat. 1 carcinogen have been greyed out, and it is probably not a very good idea to consume these. Keep in mind that the others may also have occasional typos and errors, and prolonged consumption may have detrimental health effects.</em></p>
+<?php
+  if ($termtime) {
+?>
+<p>Incomplete or unready notes are <span class="notes-item notes-item-old">greyed out</span>. The titles are <span class="notes-item notes-item-unready">bolded</span> if they have been updated since the most recent lecture.</p>
+<?php
+  } else {
+?>
+<p>Unready notes are <span class="notes-item notes-item-old">greyed out</span>.</p>
+<?php
+  }
+?>
 
 <?php include "courses_list.php"; ?>
+
+<section>
+<h1>Stuff</h1>
+<p>THESE NOTES ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, LACK OF TYPOS AND FUNNINESS OF PUNS.</p>
+
+<p>Nevertheless, please email any comments to <a href="mailto:dec41@cam.ac.uk">dec41@cam.ac.uk</a>. Feel free to point out errors or unclear explanations, as well as general typographic suggestions. Even better, send a GitHub <a href="http://github.com/dalcde/cam-notes/pulls">pull request</a>.</p>
+
+<p>Here I'd like to thank the lecturers who delivered the (usually) amazing lectures, and all of those who helpfully pointed out my mistakes and typos.</p>
+
+</section>
 <section>
 <h1>External resources</h1>
 <p>Notes from other sources I like.</p>
 <ul>
   <li><a href="http://tartarus.org/gareth/maths/notes/">Gareth Taylor's</a> notes. Mostly pure mathematics.</a></li>
   <li><a href="http://www.damtp.cam.ac.uk/user/tong/teaching.html">David Tong's</a> notes. Theoretical physics.</a></li>
-  <li><a href="http://www.damtp.cam.ac.uk/user/eal40/teach/QM2012/QMroot.html">Eugene Lim's</a> notes. IB Quantum mechanics.</a></li>
-  <li><a href="http://www.statslab.cam.ac.uk/~rrw1/">Ricahrd Weber's</a> notes. Applicable courses.</li>
-  <li><a href="http://www.archim.org.uk/notes.php">Archimedeans'</a> notes. Miscellaneous topics from different sources.</a></li>
+  <li><a href="http://www.statslab.cam.ac.uk/~rrw1/">Richard Weber's</a> notes. Applicable courses.</li>
+  <li><a href="http://www.archim.org.uk/notes.php">Archimedeans'</a> site. Miscellaneous topics from different sources.</a></li>
+  <li><a href="https://www.maths.cam.ac.uk/tripos-specific-resources">Student Representatives'</a> page. Miscellaneous topics from different sources. May overlap with above.</a></li>
 </ul>
 </section>
 
